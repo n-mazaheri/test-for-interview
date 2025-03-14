c24c0a97399bfd4e9d3f0bd7076d49178218337d
sfwdv <sfwdv@github.com>
2025-03-14 09:21:08 +0330

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState<any>(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<any>(null);
 
-  useEffect(() => {
-    async function getObjects() {
-      try {
-        setLoading(true);
-        setError(null);
-        setData(null);
-        let temp = await axios.get("https://api.restful-api.dev/objects");
-        setData(temp.data);
-        setLoading(false);
-      } catch (e) {
-        setError(e);
-        setLoading(false);
-      }
-    }
-    getObjects();
-  }, []);
   return (
     <div>
-      {loading && <div>Loading...</div>}
-      {data && data.map((item: any) => <div key={item.id}>{item.name}</div>)}
-      {error && <div>There is an error getting data from api</div>}
+hello
     </div>
   );
 }
