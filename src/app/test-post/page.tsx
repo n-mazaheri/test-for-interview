d0cd1c08ee3206f694b2a7067bd40945e4312060
sfwdv <sfwdv@github.com>
2025-03-14 09:14:40 +0330

    feat: first commit

---
+++ src/app/test-post/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+import axios from "axios";
+import { useState } from "react";
+export default function Test() {
+  const [data, setData] = useState<any>({name:"", email:""});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
+  async function sendData() {
+    setLoading(true);
+    setError(null);
+    try {
+      let temp = await axios.post("http://localhost:5000/api/users", data);
+      console.log(temp.data);
+      setLoading(false);
+    } catch (e) {
+      setError(e);
+      console.log(e);
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div>
+      <div>
+        <span>Name:</span>
+        <input
+          type="text"
+          className="border border-black rounded p-2"
+          onChange={(e) => {
+            setData({...data,name:e.target.value});
+          }}
+          value={data.name ?? ""}
+        />
+      </div>
+      <div>
+        <span>Email:</span>
+        <input
+          type="email"
+          className="border border-black rounded p-2"
+          onChange={(e) => {
+            setData({...data,email:e.target.value});
+          }}
+          value={data.email ?? ""}
+        />
+      </div>
+      <div>
+        <button className="border border-black text-black rounded p-2" onClick={sendData}>
+          {loading? "..." :"Add User"}
+        </button>
+      </div>
+      {error && <div className="text-color-red">There is error adding user</div>}
+    </div>
+  );
+}
